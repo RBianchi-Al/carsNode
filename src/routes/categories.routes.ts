95fe35e05e8ae9be76680b4319a46ab001f22225
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-16 16:59:44 -0300

    create type category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
 
-    const category = {
-        id: uuidV4(),
+    const category = new Category();
+
+    Object.assign(category, {
         name,
         description,
-    };
+        created_ad: new Date(),
+    });
+
     categories.push(category);
 
     return res.status(201).json(categories);
