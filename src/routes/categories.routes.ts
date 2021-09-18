7c095ec9a46119de53d58bbf5098be28cc14d080
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-18 18:45:16 -0300

    List Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,24 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
-
-    const category = new Category();
-
-    Object.assign(category, {
-        name,
-        description,
-        created_ad: new Date(),
-    });
-
-    categories.push(category);
-
-    return res.status(201).json(categories);
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+        return res.status(400).json({
+            error: "Category Already Exists",
+        });
+    }
+    categoriesRepository.create({ name, description });
+    return res.status(201).json(categoriesRepository);
 });
 
+categoriesRoutes.get("/", (req, res) => {
+    const catetogiesList = categoriesRepository.list();
+    return res.json(catetogiesList);
+});
 export { categoriesRoutes };
