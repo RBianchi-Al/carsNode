072db2541c38c970e96969e155c649107a4abfb5
RBianchi-Al <renatalebianchi@gmail.com>
2021-09-25 12:40:46 -0300

    Create Catalog Refactoring

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,12 @@
-import { ICategoriesRepositories } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepositories } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
     description: string;
 }
-class CreateCategoryService {
+class CreateCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepositories) {}
+
     execute({ name, description }: IRequest): void {
         const categoryAlreadyExists =
             this.categoriesRepository.findByName(name);
@@ -16,4 +17,4 @@ class CreateCategoryService {
     }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
